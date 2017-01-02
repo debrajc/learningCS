3fb0861a83620a8bcec5286eec44d52c5133539d
debrajc <debrajc@outlook.com>
2017-01-02 13:55:49 -0800

    adding bubble sort algorithm

---
+++ bubblesort.py
@@ -0,0 +1,24 @@
+# Bubble sort implementation in python
+print("Please enter numbers to generate a list; type 'END' when done.")
+list = []
+while True:
+	input = raw_input("Input: ")
+	if input == 'END':
+		break
+	try:
+		list.append(float(input))
+	except ValueError:
+		print "Invalid input! Enter a number or type 'END'..."
+	
+# Bubble sort algorithm
+def bblsort(numList):
+	for i in range(len(numList)-1):
+		for j in range(len(numList)-1-i):
+			if numList[j] > numList[j+1]:
+				temp = numList[j+1]
+				numList[j+1] = numList[j]
+				numList[j] = temp
+	return(numList)
+	
+# Call bubble sort function on input list
+print "Sorted list: " + str(bblsort(list))
